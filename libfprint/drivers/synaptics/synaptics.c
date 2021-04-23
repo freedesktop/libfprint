1e4dd69fb89ffe19b8b0c1ac9eac8b8e2e9ea397
Benjamin Berg <bberg@redhat.com>
2021-04-23 10:24:55 +0200

    synaptics: Use critical section API
    This is more for demonstration purposes. The only functional change here
    would be that the delete command cannot be cancelled.

--- libfprint/drivers/synaptics/synaptics.c
+++ libfprint/drivers/synaptics/synaptics.c
@@ -510,6 +510,12 @@ verify_msg_cb (FpiDeviceSynaptics *self,
   FpDevice *device = FP_DEVICE (self);
   bmkt_verify_resp_t *verify_resp;
 
+  if (self->action_starting)
+    {
+      fpi_device_critical_leave (device);
+      self->action_starting = FALSE;
+    }
+
   if (error)
     {
       fpi_device_verify_complete (device, error);
@@ -602,6 +608,8 @@ verify (FpDevice *device)
 
   G_DEBUG_HERE ();
 
+  self->action_starting = TRUE;
+  fpi_device_critical_enter (device);
   synaptics_sensor_cmd (self, 0, BMKT_CMD_VERIFY_USER, user_id, user_id_len, verify_msg_cb);
 }
 
@@ -628,6 +636,12 @@ identify_msg_cb (FpiDeviceSynaptics *self,
                  GError             *error)
 {
   FpDevice *device = FP_DEVICE (self);
+
+  if (self->action_starting)
+    {
+      fpi_device_critical_leave (device);
+      self->action_starting = FALSE;
+    }
 
   if (error)
     {
@@ -717,6 +731,11 @@ identify_msg_cb (FpiDeviceSynaptics *self,
 static void
 identify (FpDevice *device)
 {
+  FpiDeviceSynaptics *self = FPI_DEVICE_SYNAPTICS (device);
+
+  self->action_starting = TRUE;
+  fpi_device_critical_enter (device);
+
   init_identify_msg (device);
   compose_and_send_identify_msg (device);
 }
@@ -818,6 +837,12 @@ enroll_msg_cb (FpiDeviceSynaptics *self,
 {
   FpDevice *device = FP_DEVICE (self);
   bmkt_enroll_resp_t *enroll_resp;
+
+  if (self->action_starting)
+    {
+      fpi_device_critical_leave (device);
+      self->action_starting = FALSE;
+    }
 
   if (error)
     {
@@ -965,6 +990,9 @@ enroll (FpDevice *device)
   payload[1] = finger;
   memcpy (payload + 2, user_id, user_id_len);
 
+  self->action_starting = TRUE;
+  fpi_device_critical_enter (device);
+
   synaptics_sensor_cmd (self, 0, BMKT_CMD_ENROLL_USER, payload, user_id_len + 2, enroll_msg_cb);
 }
 
@@ -978,6 +1006,7 @@ delete_msg_cb (FpiDeviceSynaptics *self,
 
   if (error)
     {
+      fpi_device_critical_leave (device);
       fpi_device_delete_complete (device, error);
       return;
     }
@@ -992,6 +1021,7 @@ delete_msg_cb (FpiDeviceSynaptics *self,
       break;
 
     case BMKT_RSP_DEL_USER_FP_FAIL:
+      fpi_device_critical_leave (device);
       if (resp->result == BMKT_FP_DATABASE_NO_RECORD_EXISTS ||
           resp->result == BMKT_FP_DATABASE_EMPTY)
         {
@@ -1008,6 +1038,7 @@ delete_msg_cb (FpiDeviceSynaptics *self,
 
     case BMKT_RSP_DEL_USER_FP_OK:
       fp_info ("Successfully deleted enrolled user");
+      fpi_device_critical_leave (device);
       fpi_device_delete_complete (device, NULL);
       break;
     }
@@ -1042,6 +1073,7 @@ delete_print (FpDevice *device)
   payload[0] = finger;
   memcpy (payload + 1, user_id, user_id_len);
 
+  fpi_device_critical_enter (device);
   synaptics_sensor_cmd (self, 0, BMKT_CMD_DEL_USER_FP, payload, user_id_len + 1, delete_msg_cb);
 }
 
